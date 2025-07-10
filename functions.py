1cf6c1d9f5b516d23b8408cff98403e3dd6dc6d5
Lucasulinux <lcdituri@gmail.com>
2025-07-10 14:55:42 +0200

    Salvataggio di tutti i dati in un formato più leggibile, csv

--- functions.py
+++ functions.py
@@ -3,3 +3,11 @@ class Minerale:
         self.nome = nome
         self.x = x_vals
         self.y = y_vals
+
+class Minerali_da_riconoscere:
+    def __init__(self, nome, x_vals, y_vals,num_riga, num_colonna):
+        self.nome = nome
+        self.riga = num_riga
+        self.colonna = num_colonna
+        self.x = x_vals
+        self.y = y_vals
